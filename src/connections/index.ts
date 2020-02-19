7c7482fc798f499e74fa632a9403959c57a988ac
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2020-02-18 20:32:33 -0500

    sigh, fixes symbol export

--- src/connections/index.ts
+++ src/connections/index.ts
@@ -5,5 +5,5 @@ export * from './MySql.Connection.Factory';
 export * from './exceptions/Connection.Exception';
 export * from './exceptions/MySql.Exception';
 export * from './IConnection.Config';
-import * as Connection from './Connection.Symbols';
+import { ConnectionSymbols as Connection} from './Connection.Symbols';
 export { Connection }
